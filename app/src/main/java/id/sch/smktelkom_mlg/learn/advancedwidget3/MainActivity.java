a4bba445449c001171a29d993c049987d4633333
widiasatria <widiasp@gmail.com>
2016-11-03 22:23:29 +0700

    Simple Data Binding Final

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget3/MainActivity.java
@@ -0,0 +1,22 @@
+package id.sch.smktelkom_mlg.learn.advancedwidget3;
+
+import android.app.Activity;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.sch.smktelkom_mlg.learn.advancedwidget3.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity
+
+{
+    String nama;
+    ActivityMainBinding biding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        biding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
